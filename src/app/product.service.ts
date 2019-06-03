45158b7f43017ef8bd84c563525df3a0b22ff9c3
Donny Singh <donny.singh@spar.co.za>
2019-06-03 14:58:54 +0200

    Creating The Album Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import {map} from 'rxjs/operators';
   import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 
 @Injectable()
@@ -10,7 +12,7 @@ export class ProductService {
   private _albumUrl= '../assets/album.json';
   constructor( private _http: Http) { }
 
-   getAlbum( id: number) {
+   getAlbum( id: number) : Observable<Album>{
     return this._http.get(this._albumUrl)
     .pipe( map ((response) => response.json()))
   }
