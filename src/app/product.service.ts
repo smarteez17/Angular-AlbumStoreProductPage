16bb86e8497ab5948420707f47125761447d3c88
Donny Singh <donny.singh@spar.co.za>
2019-06-03 15:05:14 +0200

    Using Interfaces to Describe Data 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,6 +14,6 @@ export class ProductService {
 
    getAlbum( id: number): Observable<Album>{
     return this._http.get(this._albumUrl)
-    .pipe(  map ((response) => <Album>response.json()))
+    .pipe(map (response => <Album>response.json()))
   }
 }
